f632d155e50ec8ccc9e114995788f66e46b8a6e4
Rakheeb <rakheebshaikh906@gmail.com>
2026-08-06 13:29:51 +0530

    visual flow how this things are work

---
+++ checker.ts
@@ -0,0 +1,10 @@
+//checker in typeScript
+
+// ts code -> lexer -> parser -> blinder -> checker -> emmiter -> syntax check short circuit
+
+//parser-> ast -> blinder -> symbol tables prent pointer flow nodes
+
+
+//diagram
+//https://www.typescriptlang.org/docs/handbook/checker.html
+
